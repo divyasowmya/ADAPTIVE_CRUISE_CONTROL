a1c708bff17b7057c50035e0b862f118afcbbfdd
divyasowmya <158157685+divyasowmya@users.noreply.github.com>
2024-03-19 10:08:32 +0530

    #17625 AdaptiveCruiseControl.js
    Test alert functionality under various driving conditions to ensure effectiveness and clarity

---
+++ AdaptiveCruiseControl.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Adaptive Cruise Control</h1>
+  <script>document.write("[TASK-17625] Test alert functionality under various driving conditions to ensure effectiveness and clarity
+")</script>
+</body>
+</html>
+ 
